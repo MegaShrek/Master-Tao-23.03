8deac45e31eaf10f5be1bfadc07b6e205830b0de
MegaShrek <vladyslavsharapoff@gmail.com>
2023-07-04 22:34:05 +0300

    first-screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
     $('.first-screen-slider').slick({
         dots: true,
+        infinite: true,
+        appendArrows: '.first-screen-slider__arrows'
     });
         
   });
