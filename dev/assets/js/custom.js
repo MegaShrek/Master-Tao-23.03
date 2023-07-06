51ebafbbc36f5ddb9f61401ed7c8fd5fa33c70ec
MegaShrek <vladyslavsharapoff@gmail.com>
2023-07-06 03:10:47 +0300

    calculation-section

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -18,3 +18,18 @@ $(document).ready(function(){
           ]        
   });
 });
+
+const checkbox = document.querySelectorAll('[type="checkbox"]')
+
+checkbox.forEach(item => {
+  item.closest('.input-box').classList.add('has-checkbox')
+  item.closest('.input-column').classList.add('padding-top')
+})
+
+document.getElementById('photo').onchange = function () {
+  //short name
+  //document.getElementById('file-name').innerHTML = this.files[0].name;
+
+  //long name
+  document.getElementById('file-name').innerHTML = this.value;
+}
