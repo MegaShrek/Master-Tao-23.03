7cc9bb60c3161ee40af79d1897e3fab41037e376
MegaShrek <vladyslavsharapoff@gmail.com>
2023-07-13 01:45:30 +0300

    advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -17,6 +17,33 @@ $(document).ready(function(){
             // instead of a settings object
           ]        
   });
+  mobileOnlySlider("#advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
 });
 
 const checkbox = document.querySelectorAll('[type="checkbox"]')
@@ -33,3 +60,4 @@ document.getElementById('photo').onchange = function () {
   //long name
   document.getElementById('file-name').innerHTML = this.value;
 }
+
