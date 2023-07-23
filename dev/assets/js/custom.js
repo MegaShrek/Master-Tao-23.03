0734989df46aa4d3b6502e27c29c6f9881d071ac
MegaShrek <vladyslavsharapoff@gmail.com>
2023-07-24 02:12:46 +0300

    delivery-slider-done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -53,31 +53,25 @@ $('.slider-template').slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
+      breakpoint: 1310,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 890,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
       }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+    },
+    {
+      breakpoint: 530,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+      }
+    },
   ]
 });
 });
